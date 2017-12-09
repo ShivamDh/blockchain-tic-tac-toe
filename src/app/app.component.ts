95ec7d181ce745b30ee196a033b0f2b351353b99
Shivam Dharme <shdh8888@yahoo.ca>
2017-12-08 20:56:03 -0400

    Small inital commit, modify title and metadata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Blockchain Tic-Tac-Toe';
+  titleHeader = 'Welcome to ' + this.title + '!';
 }
