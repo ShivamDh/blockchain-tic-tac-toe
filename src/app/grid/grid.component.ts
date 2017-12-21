2afb0fec30f45d45e71c66b684b224c0035d2ebc
Shivam Dharme <shdh8888@yahoo.ca>
2017-12-20 23:41:35 -0400

    Utilize crypto-js library, test components utilizing Angular modules

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import SHA1 = require('crypto-js/sha1');
+
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.sass']
 })
 export class GridComponent implements OnInit {
+	// -1 = unfilled slot, 0 = O (letter), 1 = X (letter)
+	grids = [];
 
-  constructor() { }
+	constructor() {
+		this.grids = Array(9).fill(-1);
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+
+	}
+
+	cellClicked(id: number) {
+  		console.log(id);
+		console.log(SHA1("abc").toString());
+	}
 
 }
