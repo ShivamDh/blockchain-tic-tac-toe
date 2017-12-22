e0ed656606ae0d809f443817bc6fe4c307d2dd68
Shivam Dharme <shdh8888@yahoo.ca>
2017-12-22 16:17:50 -0400

    Add game message to indicate turn and invalid moves

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgSwitchCase } from '@angular/common';
 
 import SHA1 = require('crypto-js/sha1');
 
@@ -10,9 +11,12 @@ import SHA1 = require('crypto-js/sha1');
 export class GridComponent implements OnInit {
 	// -1 = unfilled slot, 0 = O (letter), 1 = X (letter)
 	grids = [];
+	turnNumber = 1;
+	gameMessage = "";
 
 	constructor() {
 		this.grids = Array(9).fill(-1);
+		this.gameMessage = "It's Player X's turn";
 	}
 
 	ngOnInit() {
@@ -20,8 +24,25 @@ export class GridComponent implements OnInit {
 	}
 
 	cellClicked(id: number) {
-  		console.log(id);
-		console.log(SHA1("abc").toString());
+		if (this.grids[id] == -1) {
+			if (this.turnNumber % 2) {
+				this.grids[id] = 1;
+				this.gameMessage = this.gameMessage.replace("X", "O");
+			} else {
+				this.grids[id] = 0;
+				this.gameMessage = this.gameMessage.replace("O", "X");
+
+			}
+			++this.turnNumber;
+	  		console.log(id);
+			console.log(SHA1(JSON.stringify(this.grids)).toString());
+		} else {
+			var oldMessage = this.gameMessage;
+			this.gameMessage = "Invalid cell clicked, try again";
+			setTimeout( () => {
+				this.gameMessage = oldMessage;
+			}, 1000);
+		}
 	}
 
 }
