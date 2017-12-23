1d60bb587839ca6d25fc9373ec9604922d0a9e06
Shivam Dharme <shdh8888@yahoo.ca>
2017-12-23 12:47:14 -0400

    Use a 2-D array for tic-tac-toe values, implement end game logic

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -13,9 +13,11 @@ export class GridComponent implements OnInit {
 	grids = [];
 	turnNumber = 1;
 	gameMessage = "";
+	gameOver = false;
 
 	constructor() {
-		this.grids = Array(9).fill(-1);
+		this.grids = Array(3).fill(0);
+		this.grids = this.grids.map( () => Array(3).fill(-1));
 		this.gameMessage = "It's Player X's turn";
 	}
 
@@ -23,18 +25,22 @@ export class GridComponent implements OnInit {
 
 	}
 
-	cellClicked(id: number) {
-		if (this.grids[id] == -1) {
+	cellClicked(row: number, col: number) {
+		if (this.gameOver) {
+			return;
+		}
+
+		if (this.grids[row][col] == -1) {
 			if (this.turnNumber % 2) {
-				this.grids[id] = 1;
+				this.grids[row][col] = 1;
 				this.gameMessage = this.gameMessage.replace("X", "O");
 			} else {
-				this.grids[id] = 0;
+				this.grids[row][col] = 0;
 				this.gameMessage = this.gameMessage.replace("O", "X");
-
 			}
 			++this.turnNumber;
-	  		console.log(id);
+			this.gameOver = this.isGameOver(row, col);
+	  		console.log(this.grids);
 			console.log(SHA1(JSON.stringify(this.grids)).toString());
 		} else {
 			var oldMessage = this.gameMessage;
@@ -43,6 +49,51 @@ export class GridComponent implements OnInit {
 				this.gameMessage = oldMessage;
 			}, 1000);
 		}
+	}
+
+	// Add tic-tac-toe game logic to check if the game is completed
+	// Checking 
+	isGameOver(row: number, col: number) {
+		// Game can't be over until at least 5 total turns have been completed
+		if (this.turnNumber > 5) {
+
+			/*
+				All logic used below can be applied to a tic-tac-toe board of any size	
+			*/
+
+			var newestCell = this.grids[row][col];
+
+			// Horizontal 3-in-a-row
+			if (this.grids[row].every( (e) => e === newestCell)) {
+
+				return true;
+			}
+
+			// Vertical 3-in-a-row
+			if (this.grids.every( (r) => r[col] === newestCell)) {
+
+				return true;
+			}
+			
+			if (row === col) {
+				// Diagonal (top-left to bottom-right) 3-in-a-row
+				if (this.grids.every( (r, i) => r[i] === newestCell)) {
+
+					return true;
+				}
+			}
+
+			if (row === this.grids.length - col - 1) {
+				// Anti-Diagonal (top-right to bottom-left) 3-in-a-row
+				if (this.grids.every( (r, i) => r[r.length-1-i] === newestCell)) {
+
+					return true;
+				}
+			}
+
+		}
+
+		return false;
 	}
 
 }
