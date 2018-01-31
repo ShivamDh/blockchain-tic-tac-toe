fdc6f2a07a1abe826a2dbc2e9fa4a3d28529ddf4
Shivam Dharme <shdh8888@yahoo.ca>
2018-01-31 17:33:49 -0400

    Utilizing Pusher API to send API requests and have live communication between two players

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgIf } from '@angular/common';
+import * as CryptoJS from 'crypto-js';
 
 import { GridComponent } from './grid/grid.component'
 import { GamesComponent } from './games/games.component'
+
+import * as pusherCred from '../../pusherCredentials.js';
+
+declare const Pusher: any;
 
 @Component({
   selector: 'app-root',
@@ -15,6 +20,12 @@ export class AppComponent {
 	titleHeader: string;
 	resetButtonString: string;
 	gameOver: boolean;
+	canPlay: boolean = true;
+	player: number = 0;
+	players: number = 0;
+
+	pusherChannel: any;
+	gameId: string;
 
 	@ViewChild(GridComponent)
 	private grid: GridComponent;
@@ -27,6 +38,8 @@ export class AppComponent {
 		this.titleHeader = 'Welcome to ' + this.title + '!';
 		this.gameOver = false;
 		this.resetButtonString = 'Reset Game';
+
+		this.initalizePusher();
 	}
 
 	receiveNewState($event) {
@@ -39,4 +52,51 @@ export class AppComponent {
 		this.grid.clear();
 	}
 
+	initalizePusher() {
+		let id = this.getQueryParameter('id');
+		if (!id) {
+			id = this.getUniqueId();
+			location.search = location.search ? '&id=' + id : 'id=' + id;
+		}
+		this.gameId = id;
+
+		const pusher = new Pusher(pusherCred.key, {
+			authEndpoint: './pusher/auth',
+			cluster: 'us2',
+			disableStats: true
+		});
+
+		this.pusherChannel = pusher.subscribe(this.gameId);
+
+		this.pusherChannel.bind('pusher:member_added', () => {
+			++this.players;
+		});
+
+		this.pusherChannel.bind('pusher:subscription_succeeded', (members) => {
+			this.players = members.count;
+			this.setPlayer(this.players);
+		});
+
+		this.pusherChannel.bind('pusher:member_removed', () => {
+			--this.players;
+		});
+	}
+
+	setPlayer(players:number = 0) {
+        this.player = players - 1;
+        if (players == 1) {
+          this.canPlay = true;
+        } else if (players == 2) {
+          this.canPlay = false;
+        }
+      }
+
+	getQueryParameter(param) : string {
+		var match = RegExp('[?&]' + param + '=([^&]*)').exec(location.search);
+        return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+	}
+
+	getUniqueId() : any {
+		return 'presence-' + CryptoJS.SHA1(Math.random().toString()).toString()
+	}
 }
